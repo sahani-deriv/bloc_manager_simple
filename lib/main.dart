e06e2f5634726f08e914ab49467cde08873e57f6
sahani-deriv <125638269+sahani-deriv@users.noreply.github.com>
2023-03-03 11:50:47 +0800

    feat: register cubits

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
+import 'package:bloc_manager_simple/bloc_manager/first_number/first_number_state_emitter.dart';
+import 'package:bloc_manager_simple/bloc_manager/second_number/second_number_state_emitter.dart';
 import 'package:bloc_manager_simple/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_deriv_bloc_manager/manager.dart';
+
+import 'bloc_manager/first_number/first_number_cubit.dart';
+import 'bloc_manager/second_number/second_number_cubit.dart';
 
 void main() {
+  registerBlocs();
+  setUpEventDispatcher();
   runApp(const MyApp());
 }
+
+registerBlocs() => BlocManager.instance
+  ..register(FirstNumberCubit())
+  ..register(SecondNumberCubit());
+
+setUpEventDispatcher() => StateDispatcher(BlocManager.instance)
+  ..register<FirstNumberCubit, FirstNumberStateEmitter>(
+    (BaseBlocManager blocManager) => FirstNumberStateEmitter(blocManager),
+  )
+  ..register<SecondNumberCubit, SecondNumberStateEmitter>(
+    (BaseBlocManager blocManager) => SecondNumberStateEmitter(blocManager),
+  );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
